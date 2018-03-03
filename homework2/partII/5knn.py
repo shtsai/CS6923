94f936a29a4f395cac453af33ba25371b5c8da1f
Shang-Hung (Sean) Tsai <shtsai7@users.noreply.github.com>
2018-03-02 23:09:12 -0500

    complete 5 K Nearest Neighbor

--- homework2/partII/5knn.py
+++ homework2/partII/5knn.py
@@ -67,9 +67,53 @@ class KNearestNeighbor(object):
             # print(str(point[0]) + " " + str(point[1]))
         return sum / size
 
-knn = KNearestNeighbor(100)
-knn.loadTrainingData("../auto_train.csv")
-# knn.printData()
 
-test = DataPoint(200, 200, 0)
-print(knn.predict(test))
+class KNNExperienment(object):
+    def __init__(self, k, trainingFile, testFile):
+        self.knn = KNearestNeighbor(k)
+        self.trainingFile = trainingFile
+        self.testFile = testFile
+
+    def train(self):
+        self.knn.loadTrainingData(self.trainingFile)
+
+    def test(self):
+        err = 0.0
+        with open(self.testFile) as testData:
+            data = csv.reader(testData)
+            # skip header
+            next(data, None)
+
+            for row in data:
+                displacement = float(eval(row[0]))
+                horsepower = float(eval(row[1]))
+                mpg = float(eval(row[2]))
+                testDataPoint = DataPoint(displacement, horsepower, mpg)
+                prediction = self.knn.predict(testDataPoint)
+                err += pow(prediction - mpg, 2)
+                # print("{0:f} {1:f}".format(prediction, mpg))
+        return err / 2.0
+
+
+
+def main():
+    trainingFile = "../auto_train.csv"
+    testFile = "../auto_test.csv"
+    knn1 = KNNExperienment(1, trainingFile, testFile)
+    knn1.train()
+    knn1Err = knn1.test()
+    print("K = 1, Err = {0:.2f}".format(knn1Err))
+
+    knn3 = KNNExperienment(3, trainingFile, testFile)
+    knn3.train()
+    knn3Err = knn3.test()
+    print("K = 3, Err = {0:.2f}".format(knn3Err))
+
+    knn20 = KNNExperienment(20, trainingFile, testFile)
+    knn20.train()
+    knn20Err = knn20.test()
+    print("K = 20, Err = {0:.2f}".format(knn20Err))
+
+if __name__ == "__main__":
+    main()
+
