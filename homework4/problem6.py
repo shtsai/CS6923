b12a546c6161940949378b8f234561f7b23d305c
Shang-Hung (Sean) Tsai <seantsai726@gmail.com>
2018-04-23 11:14:23 -0400

    update homework 4 notebook, add output images for problem 6

--- homework4/problem6.py
+++ homework4/problem6.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
+import sklearn.decomposition as skd
 
 from sklearn.datasets import fetch_lfw_people
 lfw_people = fetch_lfw_people(min_faces_per_person=70)
@@ -20,6 +21,34 @@ def plt_face(x):
 #     plt.subplot(1, nplt, i+1)
 #     plt_face(fea[i])
 
+# (a)
+#plt_face(fea[4])
+
+# (b)
 meanface = np.mean(fea, axis=0)
-plt_face(meanface)
+#plt_face(meanface)
+
+# (c)
+pca = skd.PCA(n_components=6)
+skd.PCA.fit(pca, fea)
+newfea = pca.transform(fea)
+print(newfea[4])
+
+# (d)
+# 6 principal components
+# pca = skd.PCA(n_components=6)
+# skd.PCA.fit(pca, fea)
+# Z = pca.transform(fea)
+# W = pca.components_
+# X6 = W.T.dot(Z.T) + np.array([meanface]).T
+# plt_face(X6.T[4])
+
+# 100 principal components
+# pca = skd.PCA(n_components=100)
+# skd.PCA.fit(pca, fea)
+# Z = pca.transform(fea)
+# W = pca.components_
+# X100 = W.T.dot(Z.T) + np.array([meanface]).T
+# plt_face(X100.T[4])
+
 plt.show()
